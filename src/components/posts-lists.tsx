73a591ccd2d31b05135512bd79f3d7ef35db458c
markopolo <veraszto9@gmail.com>
2024-10-24 13:00:39 +0200

    first commit

---
+++ src/components/posts-lists.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import prisma from "@/lib/db";
+export default async function PostsList() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const posts = await prisma.post.findMany();
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id} className="mb-3">
+          <Link href={`/posts/${post.id}`}>{post.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
